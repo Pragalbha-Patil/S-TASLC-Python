a02a05e1b522e07ce5922a72858a4f4d4a70d182
psp@psp.com <psp@psp.com>
2019-09-28 19:07:38 +0530

    rasp optimized - see errors.txt

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def func():
         
         arr=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r',
         's','t','u','v','w','x','y','z',' ']
-        with sr.Microphone() as source:
+        with sr.Microphone(device_index=0) as source:
 
                 r.adjust_for_ambient_noise(source) 
                 i=0
