104e4e5965f4f50d62cec64a75ec4c320845d06a
Pragalbha Patil <pragalbha.p@somaiya.edu>
2019-10-02 16:12:06 +0530

    secured ssh password

--- rasp_main.py
+++ rasp_main.py
@@ -10,32 +10,34 @@ import string
 import wget
 import paramiko
 import time
-
-try:
-    os.system("rm out.wav && clear")
-    ip = input("Enter the ip to recieve audio from: ")
-    user_name = input("Enter username: ")
-    pass_word = input("Enter password: ")
-    print("Connecting to host..")
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(ip, username=user_name, password=pass_word)
-    print("Connection succeeded, recording now.. Please speak..")
-    client.exec_command('rm out.wav')
-    client.exec_command('python -m SimpleHTTPServer 8888')
-    client.exec_command('arecord -d 5 -f cd -t wav out.wav')
-    time.sleep(6)
-except:
-    print("Couldn't connect to recieve audio, please make sure the host is up.")
-    quit()
+from getpass import getpass
 
 arr=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r',
         's','t','u','v','w','x','y','z',' ']
 r = sr.Recognizer()
 
-# file = askopenfilename()
-# print(file)
-url = 'http://192.168.0.100:8888/out.wav' #make sure you 've recorded and kept the audio 
+try:
+    os.system("rm out.wav && clear")
+    ip = input("Enter the ip to recieve audio from: ")
+    user_name = input("Enter username: ")
+    # pass_word = input("Enter password: ")
+    pass_word = getpass("Enter secure password: ")
+    print("Connecting to host..")
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user_name, password=pass_word)
+    print("Connection succeeded..")
+    time.sleep(2)
+    client.exec_command('rm out.wav')
+    client.exec_command('python -m SimpleHTTPServer 8888')
+    print("Please speak now..")
+    client.exec_command('arecord -d 5 -f cd -t wav out.wav')
+    time.sleep(6)
+except:
+    print("Something went wrong, please make sure the host is up.")
+    quit()
+
+url = 'http://192.168.0.100:8888/out.wav' # make sure you 've recorded and kept the audio 
 print("Downloading...")
 audio = wget.download(url)
 if(audio):
@@ -48,7 +50,11 @@ if audio.lower().endswith(('.pcm','.wav')):
         audio_text = r.record(source)
     # print(type(audio_text))
     audio = r.recognize_google(audio_text)
+    if(audio == 'goodbye'):
+        print("Gotta go")
+        quit()
     print("The transcribed audio is: "+audio)
+    print()
     print("Now converting to Sign language...")
     for i in range(len(audio)):
                                                     #a[i]=a[i].lower()
